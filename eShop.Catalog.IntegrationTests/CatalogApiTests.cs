7dff8d680f157d00b3c5ec71fc4e99b563a57270
Markonick <nicolas.markos@gmail.com>
2017-11-03 23:40:00 +0000

    completed integration tests for catalog api

--- eShop.Catalog.IntegrationTests/CatalogApiTests.cs
+++ eShop.Catalog.IntegrationTests/CatalogApiTests.cs
@@ -75,17 +75,20 @@ namespace eShop.Catalog.IntegrationTests
         }
 
         [Theory]
-        [InlineData(1, 4)]
-        [InlineData(null, 2)]
-        [InlineData(null, null)]
-        [InlineData(5, null)]
-        public async Task Client_should_be_able_to_get_list_of_items_per_typeid_or_brandid_or_both_and_return_okstatus(int? catalogTypeId, int? catalogBrandId)
+        [InlineData(2, 1)]
+        [InlineData(2, 3)]
+        public async Task Client_should_be_able_to_get_list_of_items_per_typeid_or_brandid_or_both_and_return_okstatus(int catalogTypeId, int catalogBrandId)
         {
             using (var server = new TestServer(_builder))
             {
                 var response = await server.CreateClient().GetAsync($"api/v1/catalog/items/type/{catalogTypeId}/brand/{catalogBrandId}");
 
                 response.EnsureSuccessStatusCode();
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var result = JsonConvert.DeserializeObject<PaginatedItemsViewModel<CatalogItem>>(responseBody);
+
+                Assert.Equal(1, result.Count);
             }
         }
 
@@ -112,7 +115,7 @@ namespace eShop.Catalog.IntegrationTests
         }
 
         [Fact]
-        public async Task Client_should_be_able_to_get_item_by_id_and_okstatus()
+        public async Task Get_by_id_should_be_able_to_get_item_by_id_and_okstatus()
         {
             using (var server = new TestServer(_builder))
             {
@@ -123,7 +126,7 @@ namespace eShop.Catalog.IntegrationTests
         }
 
         [Fact]
-        public async Task Client_should_return_badrequest_when_id_not_valid()
+        public async Task Get_by_id_should_return_badrequest_when_id_not_valid()
         {
             using (var server = new TestServer(_builder))
             {
@@ -135,7 +138,7 @@ namespace eShop.Catalog.IntegrationTests
         }
 
         [Fact]
-        public async Task Client_should_return_notfound_when_id_not_available()
+        public async Task Get_by_id_should_return_notfound_when_id_not_available()
         {
             using (var server = new TestServer(_builder))
             {
@@ -164,7 +167,7 @@ namespace eShop.Catalog.IntegrationTests
         }
 
         [Fact]
-        public async Task Client_should_return_500_status_when_item_invalid()
+        public async Task Post_should_return_500_status_when_item_invalid()
         {
             using (var server = new TestServer(_builder))
             {
@@ -178,7 +181,7 @@ namespace eShop.Catalog.IntegrationTests
         }
 
         [Fact]
-        public async Task Client_should_return_Created_status_when_item_updated()
+        public async Task Update_should_return_Created_status()
         {
             using (var server = new TestServer(_builder))
             {
@@ -190,15 +193,65 @@ namespace eShop.Catalog.IntegrationTests
 
                 int.TryParse(response.Headers.Location.Segments[5], out int id);
 
-
                 var updatedItem = item;
                 updatedItem.Id = id;
                 updatedItem.Price = 1111.00M;
 
                 content = new StringContent(JsonConvert.SerializeObject(updatedItem), Encoding.UTF8, "application/json");
                 response = await server.CreateClient().PutAsync(uri, content);
+                int.TryParse(response.Headers.Location.Segments[5], out int id2);
 
                 Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+                Assert.Equal(id, id2);
+            }
+        }
+
+        [Fact]
+        public async Task Update_should_return_NotFound_status_when_item_doesnt_exist()
+        {
+            using (var server = new TestServer(_builder))
+            {
+                const string uri = "api/v1/catalog/items";
+                var item = CreateCatalogItem();
+
+                var updatedItem = item;
+                //updatedItem.Id = id;
+
+                var content = new StringContent(JsonConvert.SerializeObject(updatedItem), Encoding.UTF8,
+                    "application/json");
+                var response = await server.CreateClient().PutAsync(uri, content);
+
+                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            }
+        }
+
+        [Fact]
+        public async Task Delete_should_return_NotFound_status_when_item_to_doesnt_exist()
+        {
+            using (var server = new TestServer(_builder))
+            {
+                var response = await server.CreateClient().DeleteAsync("api/v1/catalog/123456");
+
+                Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+            }
+        }
+
+        [Fact]
+        public async Task Delete_should_return_nocontent()
+        {
+            using (var server = new TestServer(_builder))
+            {
+                const string uri = "api/v1/catalog/items";
+                var item = CreateCatalogItem();
+
+                var content = new StringContent(JsonConvert.SerializeObject(item), Encoding.UTF8, "application/json");
+                var response = await server.CreateClient().PostAsync(uri, content);
+
+                int.TryParse(response.Headers.Location.Segments[5], out int id);
+                
+                response = await server.CreateClient().DeleteAsync($"api/v1/catalog/{id}");
+
+                Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);
             }
         }
 
